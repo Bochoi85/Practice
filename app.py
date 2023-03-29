535124bfc87620540ba32440ff60176cfba62522
Bochoi85 <129241364+Bochoi85@users.noreply.github.com>
2023-03-29 14:12:11 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 view = [100,150,30]
-st.write('# Youtube view')
+st.write('# Youtube view :smile:')
 st.write('## Raw')
 view 
 st.write('## Bar Chart')
@@ -8,3 +8,7 @@ st.bar_chart(view)
 import pandas as pd
 sview = pd.Series(view)
 sview
+import streamlit as st
+st.markdown('Streamlit is **_really_ cool**.')
+st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
+st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
